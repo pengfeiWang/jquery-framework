6c3ac2ab241b0d7f2acf77f9c19f376ffc74aee1
老王 <oliver@note12.com>
2015-09-10 15:42:02 +0800

    seajs

--- seajs/sea.js
+++ seajs/sea.js
@@ -18,6 +18,7 @@ seajs.config({
 		'purl': seajs._pros ? 'project/pro-' + seajs._pros[ "pro" ] + 'js/init' : ''
 	},
 	// base: 'http://127.0.0.1:8080/webstorm-project/jquery-framework-master',
+	// 
 	alias: {
 		// jquery
 		"jquery": "lib/jquery/1.9.0/jquery",
